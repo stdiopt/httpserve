9be8053b362389d31314d91f10b4717ffc07a732
luisf <stdiopt@gmail.com>
2021-05-05 21:52:27 -0300

    bump version

--- version.go
+++ version.go
@@ -2,5 +2,5 @@ package httpserve
 
 const (
   //Version contains version of the package
-  Version = "v0.0.1-4-g3538cfb - built: 2021-01-19 13:58:03 UTC"
+  Version = "v0.0.2 - built: 2021-05-06 00:52:20 UTC"
 )
